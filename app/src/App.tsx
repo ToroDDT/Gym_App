d308f0c8183c850c7f1e0dc72c7a94143d30ead7
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-24 17:50:20 -0800

    Create main components and buttons

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import SideBar from "./sideBar";
+import Button from "@mui/material/Button";
+import "./Stylesheets.css/NavBar.css";
+import "./Stylesheets.css/Dashboard.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <NavNar />
+      <DashBoard />
+    </div>
+  );
 }
 
-export default App
+function DashBoard(): JSX.Element {
+  return (
+    <div className="Dashboard">
+      <SideBar />
+
+      <div>
+        <div className="title">Dash Board</div>
+        <div className="Diary">
+          <div>Quick Add to Diary </div>
+          <HealthTrackers />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HealthTrackers(): JSX.Element {
+  type healthTrackers = "FOOD" | "EXCERCISE" | "BIOMETRICS" | "WATER" | "NOTE";
+  const Trackers: healthTrackers[] = [
+    "FOOD",
+    "EXCERCISE",
+    "BIOMETRICS",
+    "NOTE",
+    "WATER",
+  ];
+  return (
+    <div className="Trackers">
+      {Trackers.map((tracker: healthTrackers) => (
+        <Button key={tracker} variant="outlined">
+          {tracker}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+function NavNar(): JSX.Element {
+  return (
+    <nav>
+      <Button variant="outlined">Account</Button>
+    </nav>
+  );
+}
+
+export default App;
---
+++ app/src/sideBar.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Stylesheets.css/sidbar.css";
+
+export default function SideBar() {
+  return <div className="sidebar">SideBar</div>;
+}
